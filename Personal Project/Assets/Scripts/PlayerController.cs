1340ba226eadd8db4bf35c3b68bb60e71f343409
Adrian Ixta <1077056@uvu.edu>
2024-03-20 22:57:24 -0600

    Added Personal Project
     So far have scene sort of set up and movement set up used a tutorial for the camera constraint (Dynamic player movement restriction based on camera view)

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {// player movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        // calling a work function and passing the user input information and speed value
+        LocalMove(horizontal, vertical, speed);
+       // calling a movement limiter  
+        playerCamLimit();
+    }
+    // movement calculation (X-axis, Y-axis, speed)
+    void LocalMove(float x, float y, float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+    }
+   // locking player into camera boundaries
+    void playerCamLimit()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
+//Player should move Up/Down/Left/Right
+//Player will be "on rails" Meaning a predetermined path and unaffected by gravity
