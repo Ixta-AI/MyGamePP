3dce5b4aef4ecd92e95211dcea712a6255a0a1d8
Adrian Ixta <1077056@uvu.edu>
2024-05-01 19:17:01 -0600

    Created Health system & Refined PC
    Crated a function health system (more to be added)
    Refined player movement in Player controller and added beginning elements of an aiming system

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 15;
+    private Transform playerTransform;
 
+    private float speed = 10;
 
-    // Start is called before the first frame update
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     void Start()
     {
-        
+        playerTransform = transform.GetChild(0);
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
-    {// player movement
+    {// storing movement values
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Fire1")) //Finding Mouse position by left click(Shooting)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            {
+                Debug.Log(mousePos.x);
+                Debug.Log(mousePos.y);
+            }
+        }
+        if (currentHealth < 1) //Checking to see if player has died
+        {
+            Debug.Log("You are dead");
+        }
         
         // calling a work function and passing the user input information and speed value
         LocalMove(horizontal, vertical, speed);
        
         // calling a movement limiter  
         playerCamLimit();
+
+        /*PlayerMouseAim();*/
+
+        HorizontalLean(playerTransform, horizontal, 70, .1f);//Calling lean method
     }
+ 
+    void PlayerMouseAim() 
+    {
+        //Get UI elem and map it to mouse movement
+        //have player Y-axis rotation follow UI elem (with a limit similar to HorizontalLean)
+        //(Transform target, float y axis, float lookLimit, float lerpTime)
+    }
+    
     // movement calculation (X-axis, Y-axis, speed)
     void LocalMove(float x, float y, float speed)
     {
         transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+    }
+
+    void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)//Lean calculation
+    {
+        Vector3 targetEulerAngles = target.localEulerAngles; //Storing Targets current rotation in 3D Space
+        target.localEulerAngles = new Vector3(targetEulerAngles.x, targetEulerAngles.y, Mathf.LerpAngle(targetEulerAngles.z, -axis * leanLimit, lerpTime));
     }
 
    // locking player into camera boundaries
@@ -39,14 +77,22 @@ public class PlayerController : MonoBehaviour
         pos.y = Mathf.Clamp01(pos.y);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
-    private void OnCollisionEnter(Collision collision)
+
+    public void TakeDamage(int damage) //Damage function
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+    private void OnCollisionEnter(Collision collision)//Enemy collision into player event detection
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            TakeDamage(1);//calling damage function for collision
             Debug.Log("Enemy crashes into Player");
         }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)//Powerup event collision detection
     {
         if (other.gameObject.CompareTag("Powerup"))
         {
@@ -56,3 +102,4 @@ public class PlayerController : MonoBehaviour
 }
 //Player should move Up/Down/Left/Right
 //Player will be "on rails" Meaning a predetermined path and unaffected by gravity
+//player aim with mouse (implement rotation general to mouse cross hair)
